f2676fbf63628c305fa8fc07cbca60df3d2e4e79
Jeremy Squires <jeremysquires@fastmail.fm>
2019-03-19 16:52:02 -0600

    refactor routes for scalability

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,51 +1,13 @@
 const Router = require('koa-router');
-const fs = require('fs-extra');
-const path = require('path');
-const { find } = require('lodash');
 
 const { name, description, version } = require('./package.json');
+const users = require('./routes/users');
 
 const router = new Router({
   prefix: '/api/v1',
 });
 
-const userDefaultFile = path.resolve('./data/userDefault.json');
-// cache users test data, undefined, not {}, as it controls data loading
-let userDefaultDataJSON;
-
-const loadUserDefaultData = async (reload = false) => {
-  // only load if not loaded or reload called, otherwise return existing JSON
-  if (userDefaultDataJSON && !reload) {
-    return userDefaultDataJSON;
-  }
-  userDefaultDataJSON = await fs.readJSON(userDefaultFile);
-  return userDefaultDataJSON;
-};
-
-router.get('/users', async (ctx) => {
-  await loadUserDefaultData();
-  if (!userDefaultDataJSON) {
-    ctx.throw(404, 'No default users found');
-  }
-  // TODO: append user data from other source
-  ctx.body = userDefaultDataJSON;
-});
-
-router.get('/users/:login', async (ctx) => {
-  const { login } = ctx.params;
-  await loadUserDefaultData();
-  if (!userDefaultDataJSON) {
-    ctx.throw(404, 'No default users found');
-  }
-  // TODO: append user data from other source
-  const user = find(userDefaultDataJSON, {
-    "login": login
-  });
-  if (!user) {
-    ctx.throw(404, `No user ${login} found`);
-  }
-  ctx.body = user;
-});
+router.use('/users', users.routes(), users.allowedMethods());
 
 // affordances
 router.get('/', (ctx) => {
---
+++ src/api/routes/users.js
@@ -0,0 +1,47 @@
+const Router = require('koa-router');
+const fs = require('fs-extra');
+const path = require('path');
+const { find } = require('lodash');
+
+const router = new Router();
+
+const userDefaultFile = path.resolve('./data/userDefault.json');
+// cache users test data, undefined, not {}, as it controls data loading
+let userDefaultDataJSON;
+
+const loadUserDefaultData = async (reload = false) => {
+  // only load if not loaded or reload called, otherwise return existing JSON
+  if (userDefaultDataJSON && !reload) {
+    return userDefaultDataJSON;
+  }
+  userDefaultDataJSON = await fs.readJSON(userDefaultFile);
+  return userDefaultDataJSON;
+};
+
+router.get('/', async (ctx) => {
+  await loadUserDefaultData();
+  if (!userDefaultDataJSON) {
+    ctx.throw(404, 'No default users found');
+  }
+  // TODO: append user data from other source
+  ctx.body = userDefaultDataJSON;
+});
+
+router.get('/:login', async (ctx) => {
+  const { login } = ctx.params;
+  await loadUserDefaultData();
+  if (!userDefaultDataJSON) {
+    ctx.throw(404, 'No default users found');
+  }
+  // TODO: append user data from other source
+  const user = find(userDefaultDataJSON, {
+    "login": login
+  });
+  if (!user) {
+    ctx.throw(404, `No user ${login} found`);
+  }
+  ctx.body = user;
+});
+
+module.exports = router;
+
