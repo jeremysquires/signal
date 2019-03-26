4f04a620c6ebbc4b369a1ca2fb3858c28dd3ca9a
Jeremy Squires <jeremysquires@fastmail.fm>
2019-03-26 11:53:25 -0600

    add call to users api and display of users information

--- src/menuguide/src/components/User.vue
+++ src/menuguide/src/components/User.vue
@@ -1,44 +1,56 @@
 <template>
   <div class="user">
     <NavBar heading="User Profile Page" />
-    <p>
-      User Information
-    </p>
+    <div v-if="user">
+      <ul>
+        <li v-for="(value, key) in user" :key="key">
+          {{ key }}: {{ value }}
+        </li>
+      </ul>
+    </div>
+    <div v-else>
+      <ul>
+        <li v-for="chooseUser in users" :key="chooseUser.login">
+          <div>
+            <!-- TODO: add link to menu/login -->
+            <router-link :to="`/menu/${chooseUser.login}`">{{ chooseUser.login }}</router-link>
+          </div>
+          <ul>
+            <li v-for="(value, key) in chooseUser" :key="key">
+              <!-- TODO: add components for value objects like weight, height, etc. -->
+              {{ key }}: {{ value }}
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue';
+import { mapMutations, mapState, mapActions } from 'vuex';
 
 export default {
   name: 'User',
-  data() {
-    return {
-      users: [
-        {
-          family: null,
-          firstName: 'Adam',
-          lastName: 'Ministrator',
-          middleName: '',
-          age: 19,
-          weight: {
-            value: 185,
-            units: 'lbs',
-            dataSource: 'scale/wiifit',
-          },
-          height: {
-            value: (5 + 10/12),
-            units: 'ft',
-            dataSource: 'tape/manual',
-          },
-          fat: {
-            value: 20,
-            units: 'percent',
-            dataSource: 'calipers/manual',
-          },
-        },
-      ],
-    };
+  methods: {
+    ...mapMutations([
+      'setLogin',
+      'setUser',
+    ]),
+    ...mapActions([
+      'setUsers',
+    ]),
+  },
+  computed: {
+    ...mapState([
+      'login',
+      'users',
+      'user',
+    ])
+  },
+  async created() {
+    await this.setUsers();
   },
   components: {
     NavBar,
@@ -46,7 +58,7 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- TODO: add styling to make this more attractive -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
@@ -54,10 +66,12 @@ h3 {
 ul {
   list-style-type: none;
   padding: 0;
+  margin-left: 30px;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+  display: block;
+  margin: 0 40px;
+  text-align: start;
 }
 a {
   color: #42b983;
--- src/menuguide/src/store.js
+++ src/menuguide/src/store.js
@@ -1,16 +1,71 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
+// TODO: share this with api using lib
+const loadUserData = async (baseurl, login) => {
+  let response;
+  try {
+    if (login) {
+      response = await axios.get(`${baseurl}/users/${login}`);
+    } else {
+      response = await axios.get(`${baseurl}/users`);
+    }
+  } catch (error) {
+    console.error(error); // eslint-disable-line no-console
+    return;
+  }
+  return response.data;
+};
+
 export default new Vuex.Store({
   state: {
-
+    login: '',
+    user: undefined,
+    users: [
+      {
+        family: null,
+        firstName: 'Adam',
+        lastName: 'Ministrator',
+        middleName: '',
+        age: 19,
+        weight: {
+          value: 185,
+          units: 'lbs',
+          dataSource: 'scale/wiifit',
+        },
+        height: {
+          value: (5 + 10/12),
+          units: 'ft',
+          dataSource: 'tape/manual',
+        },
+        fat: {
+          value: 20,
+          units: 'percent',
+          dataSource: 'calipers/manual',
+        },
+      },
+    ],
   },
   mutations: {
-
+    setLogin(state, newLogin) {
+      state.login = newLogin;
+    },
+    setUser(state, newUser) {
+      state.user = newUser;
+    },
+    setUsers(state, newUsers) {
+      state.users = newUsers;
+    },
   },
   actions: {
-
-  },
+    async setUsers(context) {
+      const remoteUsers = await loadUserData('http://localhost:8282/api/v1');
+      if (remoteUsers && remoteUsers.length > 0) {
+        context.commit('setUsers', remoteUsers);
+      }
+    }
+  }
 });
