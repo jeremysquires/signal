e4f7acab0e11cc7e871f94808c02f4fd4e6c63ab
Jeremy Squires <jeremysquires@fastmail.fm>
2019-04-12 18:16:38 -0600

    create instance on aws successfully

--- src/deploy/scripts/packages.sh
+++ src/deploy/scripts/packages.sh
@@ -1,4 +1,12 @@
-sudo apt-get -y install vim
-sudo apt-get -y install git
-sudo apt-get -y install nodejs
-sudo apt-get -y install npm
+sudo apt-get -q -y update
+# sudo apt-get -q -y upgrade
+curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt-get -q -y install nodejs-doc nodejs
+sudo apt-get -q -y install npm
+# install NVM to manage node versions
+# only local to user, sudo nvm fails
+# curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+# source ~/.bashrc
+# nvm install 8.15.1
+
--- src/deploy/signal.tf
+++ src/deploy/signal.tf
@@ -10,50 +10,32 @@ resource "aws_instance" "signal" {
   # Copies scripts
   provisioner "file" {
     source      = "scripts"
-    destination = "~"
+    destination = "/home/ubuntu"
+    connection {
+      type     = "ssh"
+      user     = "ubuntu"
+    }
   }
 
   # Executes scripts
   provisioner "remote-exec" {
     inline = [
-      "chmod +x ~/scripts/*.sh",
-      "~/scripts/packages.sh",
-      "~/scripts/signal_install.sh",
-      "~/scripts/signal_run.sh",
+      "chmod +x /home/ubuntu/scripts/*.sh",
+      "bash --login /home/ubuntu/scripts/packages.sh",
+      "bash --login /home/ubuntu/scripts/signal_install.sh",
     ]
+    connection {
+      type     = "ssh"
+      user     = "ubuntu"
+    }
   }
-}
-
-resource "aws_security_group_rule" "allow_ssh" {
-  type            = "ingress"
-  from_port       = 22
-  to_port         = 22
-  protocol        = "tcp"
-  cidr_blocks = ["205.206.87.17/32"]
-  security_group_id = "sg-4c6cea0a"
-}
-
-resource "aws_security_group_rule" "allow_http" {
-  type            = "ingress"
-  from_port       = 80
-  to_port         = 8080
-  protocol        = "tcp"
-  cidr_blocks = ["205.206.87.17/32"]
-  security_group_id = "sg-4c6cea0a"
 }
 
 resource "aws_eip" "ip" {
   instance = "${aws_instance.signal.id}"
 }
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCypDLVjbZrmqJTDE69PLduMjJUjosnQYbzR8mvNTsY5eR532Ra470KuRNgbQtMH0AVORdUqS1KGpkVYDbZg57a2KOrN53FY8voU6Ojo13yf8pf+2YrpcfrPbcu37v51MOSwKtdBSqj788LLPIa8BGy4R/CFJFJEnZYbAsFPgWgRMeirLasb4ixb9Vy/rKE4JwYS6zQzr6A9QbAp+LvMFP4wFZRKkIaVV3s3rMxBQZitd8oySn4AUujK+h7HcuUSJFAwcOKVE8cdcwjCiZG0o0x70qCiRD4uFxiXN4JCf4nZuGy78Q0huCsAifrlFowoScTBWkkbvT/4OlRwDX9Iv5H jer@ustudio"
-}
-
 output "ip" {
   value = "${aws_eip.ip.public_ip}"
 }
-
-
 
