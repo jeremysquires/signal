cc4d6cd13c75dc08ffa805de59fa2ee5c2f83adb
Jeremy Squires <jeremysquires@fastmail.fm>
2019-04-13 18:54:31 -0600

    disown to allow background to continue to run

--- src/deploy/scripts/signal_run.sh
+++ src/deploy/scripts/signal_run.sh
@@ -1,9 +1,11 @@
 cd ~/signal/src/api
 npm run serve &
+disown
 
 cd ~/signal/src/menuguide
 # development: run serve uses CLI webpack devServer
 npm run serve &
+disown
 # production: run SPA under Node serve or nginx
 # npm install -g serve
 # serve -s dist
